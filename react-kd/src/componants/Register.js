896f17b306fa3c72d975aa1338345a87bb66068d
yamakalyan <yamakalyan6@gmail.com>
2023-02-27 15:15:49 -0800

    modified

--- react-kd/src/componants/Register.js
+++ react-kd/src/componants/Register.js
@@ -4,20 +4,23 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [name, setname] = useState("");
-  const [mail, setmail] = useState("");
   const [number, setnumber] = useState("");
+  const [mail, setmail] = useState("");
   const [passwod, setpassword] = useState("");
+  const [registerUser, setRegisteruser] = useState(false);
+  const [msg, setMsg] = useState('');
 
   const nameInput = (event)=>{
     setname(event.target.value);
   }
 
-  const mailInput=(event)=>{
-    setmail(event.target.value)
-  }
-
   const numberInput = (event)=>{
     setnumber(event.target.value)
+  }
+
+  const mailInput=(event)=>{
+    setmail(event.target.value)
+    
   }
 
   const passwordInput =(event)=>{
@@ -27,16 +30,27 @@ const Register = () => {
   const saveInputValues = (event)=>{
     
     event.preventDefault();
-
-    let newRegistration = {
-      Username : name,
-      Email : mail,
-      Mobile : number,
-      Passwod : passwod
+    
+    let register = {
+      user_name : name,
+      user_mobile : number,
+      user_email : mail,
+      user_password : passwod
     }
-    console.log(newRegistration);
-  }
+    
+    const options = {
+      method : 'POST',
+      headers : {'content-type':'application/json'},
+      body : JSON.stringify(register)
+    }
 
+    fetch('http://localhost:3120/user/register/', options)
+    .then(response =>response.json())
+    .then(data => {
+      setRegisteruser(data.server);
+      setMsg(data.message)
+      console.log(data.results)})
+  }
 
   return (
         <>
@@ -48,21 +62,29 @@ const Register = () => {
                 </div>
             <div className="col-md-6 mb-2 p-5 my-auto shadow bg-dark text-light rounded-lg">
                 <div className="text-center mb-2 m">
-
                     <h5><b>Register</b></h5><hr />
 
-        <form action="/action_page.php" onSubmit={saveInputValues}>
+                    {registerUser ? 
+                    <div className="alert alert-success" role="alert">
+                    {msg}
+                  </div>
+                    :
+                    <div className="alert alert-danger" role="alert">
+                   {msg}
+                  </div>}
+                    
+        <form onSubmit={saveInputValues}>
         <div className="form-group">
-        <input type="name" className="form-control bg-dark text-light" onChange={nameInput} id="name" placeholder="Enter UserName"/>
+        <input type="text" className="form-control bg-dark text-light" onChange={nameInput} id="name" required placeholder="Enter UserName"/>
         </div>
         <div className="form-group">
-        <input type="email" className="form-control bg-dark text-light" onChange={mailInput} id="email" placeholder="Enter @gmail.com"/>
+        <input type="number" className="form-control bg-dark text-light" onChange={numberInput} id="number" required placeholder="Enter Mobile Number"/>
         </div>
         <div className="form-group">
-        <input type="number" className="form-control bg-dark text-light" onChange={numberInput} id="number" placeholder="Enter Mobile Number"/>
+        <input type="email" className="form-control bg-dark text-light" onChange={mailInput} id="email" required placeholder="Enter @gmail.com"/>
         </div>
         <div className="form-group">
-        <input type="password" className="form-control bg-dark text-light" onChange={passwordInput} id="pwd" placeholder="Enter password"/>
+        <input type="password" className="form-control bg-dark text-light" onChange={passwordInput} id="pwd" required placeholder="Enter password"/>
         </div><hr />
         <div className="row">
           <div className="col mb-2">
